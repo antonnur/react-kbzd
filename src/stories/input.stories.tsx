feb94464bdb3fcfe7661d2e223df07fcc957f351
anton_mac <antonnur89@yande.ru>
2025-05-12 17:01:59 +0700

    sprint_02-01 (13 - Controlled input, FLUX)

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -26,9 +26,29 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
     setValue(el.value)
   }
 
-  return <><input ref={inputRef} />
+  return <><input ref={inputRef}/>
     <button onClick={save}>save</button>
     - actual value: {value}</>
+}
+
+export const ControlledInput = () => {
+  const [parentValue, setParentValue] = useState('')
+  return < input value={parentValue} onChange={(e) => setParentValue(e.currentTarget.value)}/>
+}
+
+export const ControlledCheckbox = () => {
+  const [parentValue, setParentValue] = useState(true)
+  return < input type={'checkbox'} checked={parentValue} onChange={(e) => setParentValue(e.currentTarget.checked)}/>
+}
+
+export const ControlledSelect = () => {
+  const [parentValue, setParentValue] = useState<string | undefined>('2')
+
+  return <select value={parentValue} onChange={(e) => setParentValue(e.currentTarget.value)}>
+    <option value="1">Minsk</option>
+    <option value="2">Moscow</option>
+    <option value="3">Boo</option>
+  </select>
 }
 
 export const ControlledInputWithFixedValue = () => <input value={'Controlled Input'}/>
